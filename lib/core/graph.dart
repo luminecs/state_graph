1d9b1001ff0dc5464323d7a237df2ee488876256
luminecs <wangxuke.china@gmail.com>
2024-04-25 14:56:30 +0800

    commit

---
+++ lib/core/graph.dart
@@ -0,0 +1,10 @@
+/// Graph is a simple implementation of a bi-directed
+/// graph using adjacency list. It can automatically
+/// delete nodes which become zero out-degree.
+class Graph<T> {
+  // Graph({this.name})
+
+  /// Get name from the node.
+  // final String Function(T)? name;
+  // String _name(T node) => name != null ? name!(node) : node.toString();
+}
